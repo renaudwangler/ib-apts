c5f5a21eefe0763273e431e156522e788fb5f7e2
Renaud <renaud.wangler@ib.cegos.fr>
2023-02-24 20:01:18 +0100

    paragraphes gris

--- inclus/apts.js
+++ inclus/apts.js
@@ -1,20 +1,3 @@
-function links() {
-  let courseTable=document.getElementsByTagName('table')[0];
-  Object.entries(courses).forEach(([courseId,courseTitle]) => {
-    let newCourse=courseTable.insertRow(0);
-    newCourse.id=courseId;
-    newCourse.onclick=function(){window.location=(courseId+'.html')};
-    let cell1=newCourse.insertCell(0);
-    let cell2=newCourse.insertCell(1);
-    cell1.innerHTML=courseId;
-    cell2.innerHTML=courseTitle;
-    if (localStorage.getItem('ibAPTSlastcourseView') !== undefined && localStorage.getItem('ibAPTSlastcourseView') == courseId) {
-      cell1.className='lastCourse';
-      cell2.className='lastCourse';
-    }
-  
-  });}
-
 function switchDiv(divId,titre) {
   div=document.getElementById(divId);
   if (div.style.display=='none') {
