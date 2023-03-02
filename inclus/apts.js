344a389a6b3e5d2d171b33ef1170d9815fba5dda
Renaud <renaud.wangler@ib.cegos.fr>
2023-03-02 12:14:21 +0100

    copier le code

--- inclus/apts.js
+++ inclus/apts.js
@@ -14,3 +14,18 @@ function indexLoad() {
 function stageLoad(pageName) {
   localStorage.setItem('ibAPTSlastcourseView',pageName);
 }
+
+let codeBlocks = document.querySelectorAll("code");
+codeBlocks.forEach((codeBlock) => {
+  // only add button if browser supports Clipboard API
+  if (navigator.clipboard) {
+    let copyButton = document.createElement("button");
+    code2copy = codeBlock.innerText;
+    copyButton.innerText = 'copier';
+    codeBlock.appendChild(copyButton);
+    copyButton.addEventListener("click", async () => {
+      await copyCode(code2copy);});}
+});
+async function copyCode(code2copy) {
+  await navigator.clipboard.writeText(code2copy);
+}
