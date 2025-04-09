a846dc8dbd58653cee671e3af9ebb1238677df77
Renaud <renaud.wangler@ib.cegos.fr>
2025-04-09 17:09:35 +0200

    msaz040.ps1

--- stages/msaz040.ps1
+++ stages/msaz040.ps1
@@ -17,12 +17,21 @@ if ((Get-CimInstance -ClassName Win32_OperatingSystem).ProductType -eq 2) {
 #Résolution 1600*900 pour la VM goDeploy
 set-displayResolution -width 1600 -height 900 -Force|out-null
 
+
+#Passage de la fenêtre Windows Powershell ISE en plein écran
+$type = Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);[DllImport("user32.dll")] public static extern int SetForegroundWindow(IntPtr hwnd);' -Name WindowAPI -PassThru
+$null = $type::ShowWindowAsync((get-process powershell_ise).MainWindowHandle,3)
+
+#Installation du module ib pour démonstrations
+Install-PackageProvider Nuget –force –verbose
+register-psrepository -Default
+Install-module ib -force
+$psISE.CurrentPowerShellTab.Files.Add((get-module -listavailable ib).path.replace('.psd1','.psm1'))
+$psISE.CurrentPowerShellTab.Files.RemoveAt(0)
+
 #Paramètrage plus lisible à distance pour l'ISE Windows Powershell
 $psISE.Options.Zoom = 150
 $psISE.CurrentPowerShellTab.ExpandedScript = $false
 $psISE.CurrentPowerShellTab.ShowCommands = $false
 
-#Passage de la fenêtre Windows Powershell ISE en plein écran
-$type = Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);[DllImport("user32.dll")] public static extern int SetForegroundWindow(IntPtr hwnd);' -Name WindowAPI -PassThru
-$null = $type::ShowWindowAsync((get-process powershell_ise).MainWindowHandle,3)
 #clear-host
