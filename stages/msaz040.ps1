73ac5eada7d834cc86b6c7539b78306489969adb
Renaud <renaud.wangler@ib.cegos.fr>
2025-07-10 15:40:31 +0200

    msaz040.ps1 utilisable sur toutes VM du stage

--- stages/msaz040.ps1
+++ stages/msaz040.ps1
@@ -1,3 +1,7 @@
+#Mise à jour nuget
+Install-PackageProvider Nuget –force | out-null
+register-psrepository -Default -ErrorAction silentlyContinue
+
 #Nettoyage du réseau si lançé depuis le DC
 if ((Get-CimInstance -ClassName Win32_OperatingSystem).ProductType -eq 2) {
   #La machine est un contrôleur de domaine.
@@ -15,16 +19,18 @@ if ((Get-CimInstance -ClassName Win32_OperatingSystem).ProductType -eq 2) {
         Write-Host "  ($($_.Exception.Message))." -ForegroundColor Magenta }}}
 
 #Résolution 1600*900 pour la VM goDeploy
-set-displayResolution -width 1600 -height 900 -Force|out-null
-
+if (get-Command set-displayResolution -erroraction silentlycontinue) {
+  set-displayResolution -width 1600 -height 900 -Force|out-null}
+else {
+  install-module DisplayConfig -Force
+  set-displayResolution -DisplayId ((get-DisplayInfo)[0].DisplayId) -width 1600 -height 900 }
 
 #Passage de la fenêtre Windows Powershell ISE en plein écran
 $type = Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);[DllImport("user32.dll")] public static extern int SetForegroundWindow(IntPtr hwnd);' -Name WindowAPI -PassThru
 $null = $type::ShowWindowAsync((get-process powershell_ise).MainWindowHandle,3)
 
 #Installation du module ib pour démonstrations
-Install-PackageProvider Nuget –force | out-null
-register-psrepository -Default -ErrorAction silentlyContinue
+
 Install-module ib -force
 $psISE.CurrentPowerShellTab.Files.Add((get-module -listavailable ib).path.replace('.psd1','.psm1')) | out-null
 $psISE.CurrentPowerShellTab.Files.RemoveAt(0)
